4f3a851dd6614c3f70ae6828c4684aeee017a15d
Maison <maisongalvao144@gmail.com>
2019-08-09 15:09:51 -0400

    Adicionado arquivo de teste stored procedures
    Adicionei o arquivo que testa as stored procedures utilizadas no curso.

---
+++ testaStoredProcedures.sql
@@ -0,0 +1,12 @@
+call ola_mundo;
+call mostra_numero;
+call mostra_numero_alias;
+call sp_com_funcoes;
+call sp_com_comentarios;
+call alo_mundo_2;
+call exibe_variavel;
+call tipos_de_dados;
+call data_hora_local;
+call sem_declaracao;
+call mesmo_tipo_dados;
+call atribui_valor;
